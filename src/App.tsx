84c38ad095b151a6141fb37c9845573b6e19a076
Asya Nikolskaya <nickolskaya@gmail.com>
2022-12-21 18:10:39 +0300

    fix: upd package

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import AddIcon from '@mui/icons-material/Add';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        console.log({
+            todo: data.get('todo'),
+        });
+    };
+
+    return (
+        <Container component='main' maxWidth='xs'>
+            <Box
+                sx={{
+                    marginTop: 8,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                }}
+            >
+                <Typography component='h1' variant='h2'>
+                    Todo list app
+                </Typography>
+                <Box
+                    component='form'
+                    onSubmit={handleSubmit}
+                    noValidate
+                    sx={{
+                        mt: 1,
+                        display: 'flex',
+                        alignItems: 'center',
+                    }}
+                >
+                    <TextField
+                        margin='normal'
+                        required
+                        id='todo'
+                        label='todo'
+                        autoFocus
+                    />
+                    <Button
+                        type='submit'
+                        endIcon={<AddIcon />}
+                        variant='contained'
+                        sx={{ ml: 4 }}
+                    >
+                        Add
+                    </Button>
+                </Box>
+            </Box>
+        </Container>
+    );
 }
-
-export default App;
