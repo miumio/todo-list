2c9f89bdaa3d61c277fe0e3b98959c7cabf00c9c
Asya Nikolskaya <nickolskaya@gmail.com>
2022-12-23 14:03:06 +0300

    feat: send notification to telegram

--- src/App.tsx
+++ src/App.tsx
@@ -9,12 +9,25 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 import AddIcon from '@mui/icons-material/Add';
 import { ITask } from './Interfaces';
 import TodoTask from './Components/TodoTask';
+import axios from 'axios';
 
 const theme = createTheme();
 
 const App: FC = () => {
+    const TOKEN = '5790750760:AAGQLjHctAIeg30rJJzoQtQWIZ94K5GQ8Ic';
+    const CHAT_ID = '-1001658794705';
+    const URL_API = `https://api.telegram.org/bot${TOKEN}/sendMessage`;
+    
     const [task, setTask] = useState<string>('');
     const [desc, setDesc] = useState<string>('');
+    
+    function sendMessage(msg: string) {
+        axios.post(URL_API, {
+            chat_id: CHAT_ID,
+            parse_mode: 'html',
+            text: msg
+        });
+    }
     const [todoList, setTodoList] = useState<ITask[]>([]);
 
     useEffect(() => {
@@ -33,21 +46,34 @@ const App: FC = () => {
     const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
         event.preventDefault();
 
+        if (!task) {
+            return;
+        }
+
+        let msg = `New task added: <b>${task}</b>`;
+        console.log(msg);
+
         const newTask = { taskName: task, taskDesc: desc };
         setTodoList([...todoList, newTask]);
         localStorage.setItem(
             'todoList',
             JSON.stringify([...todoList, newTask]),
         );
+
+        sendMessage(msg);
+
         setTask('');
         setDesc('');
     };
 
     const completeTask = (el: string): void => {
         const elements = todoList.filter((task) => task.taskName !== el);
+        let msg = `Task <b>${el}</b> is finished`;
 
         setTodoList(elements);
         localStorage.setItem('todoList', JSON.stringify(elements));
+        sendMessage(msg);
+
     };
 
     return (
@@ -97,7 +123,7 @@ const App: FC = () => {
                             margin='normal'
                             fullWidth
                             name='desc'
-                            label='Tasc Desc'
+                            label='Task Desc'
                             id='desc'
                             onChange={handleInput}
                             value={desc}
